36a45ab1e4d1a11debb482dda3780dc754c97031
Duv'orynn <davorin.matus@gmail.com>
2023-04-01 07:50:57 +0200

    Initial Commit

--- Python/100_Days_of_Code-The_Complete_Python_Pro_Bootcamp_for_2022/Day_070/capstone_project-blog_with_users_end/main.py
+++ Python/100_Days_of_Code-The_Complete_Python_Pro_Bootcamp_for_2022/Day_070/capstone_project-blog_with_users_end/main.py
@@ -14,7 +14,6 @@ from flask_gravatar import Gravatar
 from functools import wraps
 from typing import List
 import os
-from sqlalchemy import dialects
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
