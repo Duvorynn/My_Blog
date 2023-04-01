0b0433334891f99cefd6ea55b6fc674cccf9e066
Duv'orynn <davorin.matus@gmail.com>
2023-04-01 08:31:35 +0200

    Initial Commit

--- Python/100_Days_of_Code-The_Complete_Python_Pro_Bootcamp_for_2022/Day_070/capstone_project-blog_with_users_end/main.py
+++ Python/100_Days_of_Code-The_Complete_Python_Pro_Bootcamp_for_2022/Day_070/capstone_project-blog_with_users_end/main.py
@@ -16,14 +16,12 @@ import os
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-# app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 app.config["CKEDITOR_PKG_TYPE"] = "full-all"
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # CONNECT TO DB
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite://blg.db")
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
